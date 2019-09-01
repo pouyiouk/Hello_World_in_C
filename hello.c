682e2ac650748e16b023ee58d0c9abf2e3bf5d4b
C Pouyioukka <54781751+pouyiouk@users.noreply.github.com>
2019-09-01 23:38:41 +0100

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+	{
+		printf("Hello, world!\n");
+		printf("This is my initial step towards the programming world..\n");
+		printf("Let the journey begin..\n");
+	}
+
